94b9a4cd5d49956aee4ba5c86ce29002d9552274
Pankaj Vishwakarma <pankaj.vishwakarma@volox.com>
2026-03-26 19:03:47 +0530

    fix the error

--- tests/uiBasictest.spec.ts
+++ tests/uiBasictest.spec.ts
@@ -1,7 +1,13 @@
 const {test} = require('@playwright/test');
 
 
-test('Firs Playwright Test', async ({browser}) => {
-    const context = browser.newContext()
-    await context.close();
+test('Open Google and close browser', async ({ page }) => {
+
+    // Open Google
+    await page.goto('https://www.google.com');
+
+    // Wait for 3 seconds (just to see it open)
+    await page.waitForTimeout(3000);
+
+    // Browser will automatically close after test ends
 });
