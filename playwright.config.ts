a8242e28007543a8a2773b094137fa24ad5b142f
Pankaj Vishwakarma <pankaj.vishwakarma@volox.com>
2026-03-27 10:38:54 +0530

    test: implement context-based and fixture-based Playwright test cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,11 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
+  timeout: 40 * 1000,
+  expect: {
+    timeout: 40 * 1000,
+  },
+
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -39,15 +44,15 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
+    //
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     /* Test against mobile viewports. */
     // {
--- tests/uiBasictest.spec.ts
+++ tests/uiBasictest.spec.ts
@@ -1,6 +1,12 @@
 import { test } from '@playwright/test';
 
-test('First Playwright Test', async ({ browser }) => {
-    const context = await browser.newContext(); // ✅ FIX
-    await context.close();
+test('Browser Context Playwright Test', async ({ browser }) => {
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+});
+
+
+test('Page Playwright Test', async ({page }) => {
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
 });
